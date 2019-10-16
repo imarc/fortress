fd8ebce620b17c460696e9fbc4335a093c9affd5
Matthew J. Sahagian <matthew.sahagian@gmail.com>
2019-10-16 02:39:03 -0700

    Adding a bunch of comments

--- src/interfaces/UserProvider.php
+++ src/interfaces/UserProvider.php
@@ -7,8 +7,41 @@ use Psr\Http\Message\ResponseInterface as Response;
 
 interface UserProvider
 {
-	public function getName();
+	/**
+	 * Get the name for the provider
+	 *
+	 * @return string The name of the provider
+	 */
+	public function getName(): string;
+
+
+	/**
+	 * Initialize the user provider and return a response.
+	 *
+	 * The initialization function should look for information in the request
+	 * upon which it can set the token `$this->gateway->setToken()`.  Some
+	 * providers may return different responses based on the request.  For
+	 * example, an oauth style provider may look for a code parameter.  If
+	 * a code is not provided, it may return a redirect to the oauth server
+	 * for login containing client_id and such.
+	 */
 	public function initialize(Request $request): Response;
+
+
+	/**
+	 * Resolve a user from a token.
+	 *
+	 * Note: If there are cases where `initialize()` does not throw an
+	 * exception, you will almost invariably want to handle the possibility of
+	 * a `NULL` token.  In this case, you can safely return `NULL`.
+	 *
+	 * @return mixed A user ID or user data to be mapped, NULL if invalid.
+	 */
 	public function resolve($token);
+
+
+	/**
+	 *
+	 */
 	public function setGateway(Gateway $gateway): UserProvider;
 }
--- src/interfaces/UserResolver.php
+++ src/interfaces/UserResolver.php
@@ -4,5 +4,13 @@ namespace Fortress;
 
 interface UserResolver
 {
+	/**
+	 * Fetch the user based on their ID
+	 *
+	 * If the user is not logged in the `$id` will be `NULL`
+	 *
+	 * @param mixed $id The ID as provided by the gateway
+	 * @return mixed A user object or whatever your system considers a user
+	 */
 	public function fetch($id);
 }
